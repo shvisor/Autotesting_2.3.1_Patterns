43d61dbeda41c9ba453caecb2e34cfc87156ebac
Max Shvagerus <mshvagerus@gmail.com>
2023-08-13 23:28:31 +0300

    add negative tests

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -8,6 +8,7 @@ import ru.netology.delivery.data.DataGenerator;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -161,5 +162,95 @@ class DeliveryTest {
         $("[data-test-id='replan-notification'] .button").click();
         $("[data-test-id='success-notification'] .notification__title").shouldBe(visible).shouldBe(exactText("Успешно!"));
         $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldBe(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/negativecity.csv", delimiter = '|')
+    void shouldNegativeCityTest(String city, String fail) {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        $("[data-test-id='city'] input").val(city);
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='city'] .input__sub").shouldBe(visible).shouldBe(exactText(fail));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/negativename.csv", delimiter = '|')
+    void shouldNegativeNameTest(String name, String fail) {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(name);
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='name'] .input__sub").shouldBe(visible).shouldBe(exactText(fail));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/negativephone.csv", delimiter = '|')
+    void shouldNegativePhoneTest(String phone, String fail) {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(phone);
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='phone'] .input__sub").shouldBe(visible).shouldBe(exactText(fail));
+    }
+    // TODO: написать репорт. проходят 8 и меньшее количество цифр
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/negativedate.csv", delimiter = '|')
+    void shouldNegativeDateTest(int gap, String pattern, String fail) {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var firstMeetingDate = LocalDate.now().plusDays(gap).format(DateTimeFormatter.ofPattern(pattern));
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='date'] .input__sub").shouldBe(visible).shouldBe(exactText(fail));
+    }
+
+    @Test
+    void shouldDateNotSelected() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='date'] .input__sub").shouldBe(visible).shouldBe(exactText("Неверно введена дата"));
+    }
+
+    @Test
+    void shouldWithoutCheckbox() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $(".button").click();
+        $("[data-test-id='agreement']").shouldBe(visible).shouldBe(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
     }
 }
